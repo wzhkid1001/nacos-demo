8d9929571fce7443cd092b65bc40920061978320
wzhkid1001 <wzhkid1001@163.com>
2020-02-20 16:44:10 +0800

    配置分组实现

---
+++ nacos-ats/src/main/java/com/wzh/nacos/ats/controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.wzh.nacos.ats.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HelloWorldController {
+    @Value("${nacos.config}")
+    private String config;
+
+    @GetMapping("/helloNacos")
+    public String helloNacos() throws Exception {
+//        throw new Exception();
+//        Thread.sleep(60000L);
+        return "helloNacos";
+
+    }
+
+    @GetMapping("/getValue")
+    public String getValue() {
+        return config;
+    }
+}
