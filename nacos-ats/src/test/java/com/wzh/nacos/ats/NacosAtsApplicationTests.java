975ea8507abe772d1d5e5b672de69da2fbdecaa7
wzhkid1001 <wzhkid1001@163.com>
2020-02-20 16:47:25 +0800

    配置分组实现

---
+++ nacos-ats/src/test/java/com/wzh/nacos/ats/NacosAtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzh.nacos.ats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosAtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
